d08b8ceb84ba00d8fb1ec349f0a0aaac743f9d68
Ista2000 <mishra.istasis@gmail.com>
2025-04-19 20:39:41 +0530

    Show recommended jobs on the homepage UI

---
+++ frontend/app/components/JobCard.tsx
@@ -0,0 +1,205 @@
+"use client";
+import {
+  Box,
+  Text,
+  Flex,
+  Badge,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Stack,
+  Button,
+  Wrap,
+  WrapItem,
+  HStack,
+  Icon,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { AtSignIcon, CalendarIcon, ExternalLinkIcon, InfoOutlineIcon, StarIcon, TimeIcon, ViewIcon } from "@chakra-ui/icons";
+import { Global } from "@emotion/react";
+
+export interface JobType {
+    company: string;
+    description: string;
+    id: number,
+    is_active: boolean;
+    location: string;
+    posted_at: string;
+    required_experience: number;
+    role: string;
+    salary_currency: string;
+    salary_from_levels_fyi: boolean;
+    salary_max: number;
+    salary_min: number;
+    source: string;
+    title: string;
+    url: string;
+  }
+
+interface JobCardProps {
+  job: JobType;
+}
+
+const JobCard = ({ job }: JobCardProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const bgSection = useColorModeValue("gray.50", "gray.700");
+  const textColor = useColorModeValue("gray.700", "gray.200");
+  const cardBg = useColorModeValue("white", "gray.800");
+  const descriptionBg = useColorModeValue("gray.50", "gray.700");
+  const descriptionTextColor = useColorModeValue("gray.700", "gray.100");
+  job.description = job.description.replaceAll('<h1>', '<h1 style="font-size:18px">');
+  job.description = job.description.replaceAll('<p>', '<p style="margin-top:12px">');
+
+  return (
+    <>
+      <Box
+        p={4}
+        borderWidth="1px"
+        borderRadius="2xl"
+        boxShadow="md"
+        _hover={{ boxShadow: "xl", cursor: "pointer" }}
+        transition="0.3s"
+        onClick={onOpen}
+        margin="24px 0"
+      >
+        <Flex justify="space-between" mb={2}>
+          <Text fontWeight="bold" fontSize="lg">
+            {job.title}
+          </Text>
+          <Badge colorScheme={job.is_active ? "green" : "red"}>
+            {job.is_active ? "Active" : "Closed"}
+          </Badge>
+        </Flex>
+        <Text fontSize="md" color="gray.600">
+          {job.company} · {job.location}
+        </Text>
+        <Text fontSize="sm" color="gray.500" mt={1}>
+          {job.required_experience}+ yrs · {job.role} · {job.salary_currency} {job.salary_min} - {job.salary_max}
+        </Text>
+        <Text fontSize="2xs" color="gray.500" mt={1}>
+          From {job.source}
+        </Text>
+      </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="5xl" isCentered>
+        <ModalOverlay />
+        <ModalContent borderRadius="2xl" p={4}>
+          <ModalHeader>{job.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Stack spacing={6}>
+              {/* Job Overview Section */}
+              <Box bg={bgSection} p={4} borderRadius="md" boxShadow="sm">
+                <Text fontSize="lg" fontWeight="bold" mb={3} color={textColor}>
+                  Job Overview
+                </Text>
+                <Wrap spacing="12px">
+                  <WrapItem>
+                    <Box px={4} py={2} bg={cardBg} borderRadius="md" boxShadow="sm">
+                      <HStack>
+                        <Icon as={InfoOutlineIcon} color="teal.400" />
+                        <Text fontWeight="medium" color={textColor}>Role:</Text>
+                        <Text color={textColor}>{job.role}</Text>
+                      </HStack>
+                    </Box>
+                  </WrapItem>
+                  <WrapItem>
+                    <Box px={4} py={2} bg={cardBg} borderRadius="md" boxShadow="sm">
+                      <HStack>
+                        <Icon as={TimeIcon} color="blue.400" />
+                        <Text fontWeight="medium" color={textColor}>Experience:</Text>
+                        <Text color={textColor}>{job.required_experience} yrs</Text>
+                      </HStack>
+                    </Box>
+                  </WrapItem>
+                  <WrapItem>
+                    <Box px={4} py={2} bg={cardBg} borderRadius="md" boxShadow="sm">
+                      <HStack>
+                        <Icon as={CalendarIcon} color="purple.400" />
+                        <Text fontWeight="medium" color={textColor}>Posted:</Text>
+                        <Text color={textColor}>
+                          {new Intl.DateTimeFormat('en-US', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                          }).format(new Date(job.posted_at))}
+                        </Text>
+                      </HStack>
+                    </Box>
+                  </WrapItem>
+                  <WrapItem>
+                    <Box px={4} py={2} bg={cardBg} borderRadius="md" boxShadow="sm">
+                      <HStack>
+                        <Icon as={AtSignIcon} color="orange.400" />
+                        <Text fontWeight="medium" color={textColor}>Location:</Text>
+                        <Text color={textColor}>{job.location}</Text>
+                      </HStack>
+                    </Box>
+                  </WrapItem>
+                  <WrapItem>
+                    <Box px={4} py={2} bg={cardBg} borderRadius="md" boxShadow="sm">
+                      <HStack>
+                        <Icon as={StarIcon} color="green.400" />
+                        <Text fontWeight="medium" color={textColor}>Salary:</Text>
+                        <Text color={textColor}>
+                          {job.salary_currency} {job.salary_min}–{job.salary_max}
+                        </Text>
+                      </HStack>
+                    </Box>
+                  </WrapItem>
+                  <WrapItem>
+                    <Box px={4} py={2} bg={cardBg} borderRadius="md" boxShadow="sm">
+                      <HStack>
+                        <Icon as={ViewIcon} color="gray.400" />
+                        <Text fontWeight="medium" color={textColor}>Source:</Text>
+                        <Text color={textColor}>{job.source}</Text>
+                      </HStack>
+                    </Box>
+                  </WrapItem>
+                </Wrap>
+              </Box>
+
+              {/* Job Description */}
+              <Box>
+                <Text fontSize="lg" fontWeight="bold" mb={2} color={textColor} paddingLeft={4}>
+                  Description
+                </Text>
+                <Box
+                  bg={descriptionBg}
+                  p={4}
+                  borderRadius="md"
+                  boxShadow="sm"
+                  fontSize="sm"
+                  color={descriptionTextColor}
+                  maxHeight="400px"
+                  overflowY="auto"
+                  dangerouslySetInnerHTML={{ __html: job.description }}
+                />
+              </Box>
+
+              {/* External Link */}
+              <Flex justify="end">
+                <Button
+                  mt={2}
+                  colorScheme="teal"
+                  rightIcon={<ExternalLinkIcon />}
+                  as="a"
+                  href={job.url}
+                  target="_blank"
+                >
+                  View Original Posting
+                </Button>
+              </Flex>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default JobCard;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,56 @@
 "use client"
-import { Box, Card, Container } from "@chakra-ui/react";
+import { Card, Center, Container, Divider, Skeleton, Spinner, Text } from "@chakra-ui/react";
 import { AuthProvider } from "./context/AuthContext";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import JobCard, { JobType } from "./components/JobCard";
 
 const Home = () => {
+  const [jobs, setJobs] = useState<Array<JobType> | null>(null);
+  useEffect(() => {
+    const getJobs = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/job/recommended`,
+        )
+        setJobs(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getJobs();
+  }, []);
   return (
     <Container alignItems="center" margin="1% 15%" width="70%" maxWidth="100%">
-      <Card padding="12px">
-        <div>
-          Home
-        </div>
-      </Card>
+      <Text fontSize="lg">Recommended jobs according to your resume</Text>
+      <Divider margin="24px 0"/>
+        {
+          jobs?.map(job => <JobCard key={job.id} job={job} />) ||
+          <>
+            <Skeleton
+              p={4}
+              borderRadius="2xl"
+              margin="24px 0"
+              isLoaded={!!jobs}
+              height="137px"
+            />
+            <Skeleton
+              p={4}
+              borderRadius="2xl"
+              margin="24px 0"
+              isLoaded={!!jobs}
+              height="137px"
+            />
+            <Skeleton
+              p={4}
+              borderRadius="2xl"
+              margin="24px 0"
+              isLoaded={!!jobs}
+              height="137px"
+            />
+          </>
+        }
     </Container>
   );
 }
